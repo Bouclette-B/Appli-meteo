7cbcff8248832eedd11a6c51e9dc578226f01bcb
Bouclette-B <anne.boucle@lilo.org>
2020-07-30 16:27:57 +0200

    unfction updateMeteo and search btn ok, starting carousel

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
 const $currentTemp = document.querySelector('.current-temp');
-const $city = document.querySelector('.city');
-let city = 'toulon';
-let url = 'https://prevision-meteo.ch/services/json/' + city;
+const $citySearch = document.querySelector('.city-search');
+const $cityName = document.querySelector('.city-name');
+const $condition = document.querySelector('.condition');
+const $bigLogo = document.querySelector('.big-logo');
+const $body = document.querySelector('body');
 
-function updateMeteo(){
-fetch(url)
-.then(response => response.json())
-.then(function(response) {
-    $city.value = response.city_info.name;
-    $currentTemp.innerHTML = response.current_condition.tmp +'°C';
-})
-.catch(function () {
-    alert('Erreur');
-})
+let city ="Toulon" ;
+let condition =""
+const updateBackground = function (){
+    if(condition == 'Ensoleillé'){
+        $body.style.backgroundImage = 'url(assets/bg_bluesky.webp)';
+    } else if (condition == 'Développement nuageux' || condition == 'Ciel voilé' || condition == 'Eclaircies' || condition == 'Faiblement nuageux'){
+        $body.style.backgroundImage = 'url(assets/bg_cloudy_bluesky.jpg)';
+    } else if (condition == 'Fortement nuageux') {
+        $body.style.backgroundImage = 'url(assets/bg_cloudy.jpg)';
+    }
 }
 
+const updateWeather = function () {
+    if($citySearch.value != "") {
+        city = $citySearch.value
+    }
+    let url = 'https://prevision-meteo.ch/services/json/' + city;
+    fetch(url)
+    .then(response => response.json())
+    .then(function(response) {
+        $cityName.innerHTML = response.city_info.name;
+        $currentTemp.innerHTML = response.current_condition.tmp +'°C';
+        let bigLogoSrc = response.current_condition.icon_big;
+        $bigLogo.innerHTML = '<img src=' + bigLogoSrc +'>';
+        condition = response.current_condition.condition
+        $condition.innerHTML = condition;
+        $citySearch.value = "";
+        updateBackground();
+    })
+    .catch(function () {
+        alert('Choisis une vraie ville patate');
+    })
+    
+}
 
+$citySearch.addEventListener('change', updateWeather);
 
-
-
-// currentTemp.then(function (response) {
-//     currentTemp = JSON.parse(response);
-//     return currentTemp;
-// })
-// .then(function () {
-//     $currentTemp.innerHTML = currentTemp.current_condition.tmp;
-// })
+updateWeather();
